08106a5e5590681d7b30fb8e52d09cf9bddfb255
AyushGurg <133072981+hsuyaGurug@users.noreply.github.com>
2023-12-23 22:52:47 -0500

    exercise 2

--- part 1/1.1 course information step1/src/App.jsx
+++ part 1/1.1 course information step1/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>{props.p1} {props.p1Total}</p>
+      <p>{props.p2} {props.p2Total}</p>
+      <p>{props.p3} {props.p3Total}</p>
     </>
   )
 }
 
+const Total = (props) => {
+  <p>Number of exercises {props.Total}</p>
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content p1={part1} p2={part2} p3={part3} p1Total={exercises1} p2Total={exercises2} p3Total={exercises3} />
+      <Total Total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
 export default App
--- part 1/1.1 course information step1/src/main.jsx
+++ part 1/1.1 course information step1/src/main.jsx
@@ -1,30 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
 
-const Hello = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
-
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
-  )
-}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
