74cedc4563093a4c5a855763005cfd421d946adc
AyushGurg <133072981+hsuyaGurug@users.noreply.github.com>
2023-12-26 17:23:43 -0500

    1.6 finish

---
+++ part 1/part1.6-1.14/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+import Button from './Button'
+
+function App() {
+  const [good, setGood] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+
+  return (
+    <>
+      <h1>Give Feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <h2>Statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </>
+  )
+}
+
+export default App
---
+++ part 1/part1.6-1.14/src/Button.jsx
@@ -0,0 +1,8 @@
+function Button(props) {
+
+    return (
+        <button onClick={props.handleClick}>{props.text}</button>
+    )
+}
+
+export default Button
